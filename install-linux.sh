d86283d62b3ed0911af54945fbef2c28a7308ee5
AnhNT <anhnt@pop-os.lan>
2024-03-05 20:26:34 +0700

    Add linux install

---
+++ install-linux.sh
@@ -0,0 +1,30 @@
+sudo apt install tmux
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Starship
+
+curl -sS https://starship.rs/install.sh | sh
+
+# rbenv
+sudo apt install rbenv
+
+# nodenv
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+
+# tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# zsh -jump
+git clone https://github.com/agkozak/zsh-z.git ~/.zprezto-contrib/zsh-z
+
+echo "Installing rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "Installing rust apps..."
+cargo install bob-nvim
+bob install stable
+bob use stable
