3c1365a49d80e74e1b07c4fa216c0ee06de22c00
Anh Nguyen <anh.nt@josys.com>
2025-02-02 13:26:24 +0700

    Update .config/mise/config.toml Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lsp.lua Update .tmux.conf

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,18 +1,1 @@
-return {
-	{
-		"neovim/nvim-lspconfig",
-		---@class PluginLspOpts
-		opts = {
-			---@type lspconfig.options
-			servers = {
-				ruby_lsp = {
-					enabled = false,
-				},
-				solargraph = {
-					enabled = lsp == "solargraph",
-				},
-			},
-			inlay_hints = { enabled = false },
-		},
-	},
-}
+return {}
