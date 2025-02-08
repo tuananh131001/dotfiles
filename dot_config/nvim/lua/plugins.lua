3c7c669f2bd25fc3fc7bf6b1dc66c46546d4ab4e
Anh Nguyen <anh.nt@josys.com>
2025-02-08 12:24:45 +0700

    Remove .config/kitty/kitty.conf Remove .config/mise/config.toml Remove .config/nvim/.DS_Store Remove .config/nvim/dump.rdb Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lua/plugins.lua Remove .config/nvim/lua/plugins/arrow.lua Remove .config/nvim/lua/plugins/conform.lua Remove .config/nvim/lua/plugins/flash.lua Remove .config/nvim/lua/plugins/git.lua Remove .config/nvim/lua/plugins/gitsigns.lua Remove .config/nvim/lua/plugins/indent.lua Remove .config/nvim/lua/plugins/lazygit.lua Remove .config/nvim/lua/plugins/legendary.lua Remove .config/nvim/lua/plugins/lsp.lua Remove .config/nvim/lua/plugins/lualine.lua Remove .config/nvim/lua/plugins/markdown.lua Remove .config/nvim/lua/plugins/mason-tool-installer.lua Remove .config/nvim/lua/plugins/navigator.lua Remove .config/nvim/lua/plugins/neogit.lua Remove .config/nvim/lua/plugins/neotest.lua Remove .config/nvim/lua/plugins/neotree.lua Remove .config/nvim/lua/plugins/noice.lua Remove .config/nvim/lua/plugins/nvim-cmp.lua Remove .config/nvim/lua/plugins/telescope.lua Remove .config/nvim/lua/plugins/tpope.lua Remove .config/nvim/lua/plugins/vim-test.lua Remove .config/nvim/lua/plugins/yanky.lua Remove .zpreztorc Remove .zshrc

--- dot_config/nvim/lua/plugins.lua
+++
@@ -1,4 +0,0 @@
-
-return {
-  { import = "plugins" },  -- This will load all plugins in the plugins directory
-}
--- dot_config/nvim/lua/plugins/gitsigns.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "tpope/vim-fugitive" },
-  {
-    "lewis6991/gitsigns.nvim",
-		event = "VeryLazy",
-  }
-}
