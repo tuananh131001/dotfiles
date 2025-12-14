c810bd44586c441241ef03c26bf89a86918835dc
anhnt <tuananh131001@gmail.com>
2025-12-14 10:35:06 +0700

    Add mapping ghostty for linux, tmux continuum using ghostty fork

--- run_once-packages.sh
+++ run_once-packages.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 TPM_DIR="$HOME/.tmux/plugins/tpm"
+CONTINUUM_TPM_DIR="$HOME/.tmux/plugins/tmux-continuum"
 
 if [ -d "$TPM_DIR" ]; then
   echo "TPM already exists, skipping clone."
 else
   git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+
+  if [ -d "$CONTINUUM_TPM_DIR" ]; then
+    echo "Ghostty fork continuum already exists, skipping clone."
+  else
+    git clone https://github.com/zuxfoucault/tmux-continuum_dev.git "$CONTINUUM_TPM_DIR"
+  fi
 fi
