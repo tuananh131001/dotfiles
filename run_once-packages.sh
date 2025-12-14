38b7cd12aacfa18e3ddd170b3dee9665abb9dca8
Anh Nguyen <anh.nt@raksul.com>
2025-12-14 10:29:25 +0700

    Prevent cloning TPM if it already exists
    Add check to avoid cloning TPM if it already exists.

--- run_once-packages.sh
+++ run_once-packages.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+if [ -d "$TPM_DIR" ]; then
+  echo "TPM already exists, skipping clone."
+else
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+fi
