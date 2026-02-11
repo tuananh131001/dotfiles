d7df4a085df72a54098489fab96dd2f502150775
Anh Nguyen <anh.nt@raksul.com>
2026-02-11 15:34:48 +0700

    Use ghostty for tmux continuum

--- run_once-packages.sh
+++ run_once-packages.sh
@@ -13,4 +13,5 @@ if [ -d "$CONTINUUM_TPM_DIR" ]; then
   echo "Ghostty fork continuum already exists, skipping clone."
 else
   git clone https://github.com/zuxfoucault/tmux-continuum_dev.git "$CONTINUUM_TPM_DIR"
+  git checkout -b origin/feature/ghostty
 fi
