89641db27ba7e8e7bc47255d7864d39169c22e1a
Vy Phạm <vypham@Vys-MacBook-Air.local>
2026-04-30 16:14:21 +0700

    Update homebrew bin

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
+export PATH="/opt/homebrew/bin:$PATH"
 starship init fish | source
 zoxide init fish | source
 export EDITOR="nvim"
