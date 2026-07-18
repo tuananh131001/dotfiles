e063e0d58b27e0262723c71561cbfa2bea6c6ba3
Tuan Anh <tuananh131001@gmail.com>
2026-07-18 09:55:15 +0700

    Adding fish to path

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,6 @@ export PATH="/opt/homebrew/bin:$PATH"
 starship init fish | source
 zoxide init fish | source
 export EDITOR="nvim"
+export PATH="$HOME/.local/bin:$PATH"
+eval "$(mise activate fish)"
+alias claude-danger="claude --dangerously-skip-permissions"
